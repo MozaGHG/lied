efc1e75e78aa3c4b47ffbaf35e34e3df6495fe14
MozaGHG <moritz.stolt@gmail.com>
2025-02-13 10:17:52 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    music.playMelody("C5 C B D A E G F ", 300)
+    music.playMelody("G F A E B D C5 C ", 300)
+    music.playMelody("C A E D G - B C ", 300)
+    music.playMelody("A - C - E - F C ", 300)
+    music.playMelody("C5 B A G F E D C ", 300)
+    music.playMelody("- - - - - - - - ", 300)
+})
