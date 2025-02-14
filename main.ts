4ea69eeb42377c601406fe026d0ba59625b8465f
MozaGHG <moritz.stolt@gmail.com>
2025-02-14 14:38:12 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    music.playMelody("C5 C B D A E G F ", 300)
-    music.playMelody("G F A E B D C5 C ", 300)
-    music.playMelody("C A E D G - B C ", 300)
-    music.playMelody("A - C - E - F C ", 300)
-    music.playMelody("D G - E - F D E ", 300)
+basic.forever(function () {
+    music.playMelody("C - E G C - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
 })
